353164c94b7e29ac3ba7f2bb0addc99c61a5cb6b
Renee127 <64703105+renee127@users.noreply.github.com>
2020-10-06 21:13:32 -0400

    minor syntax improvements

--- flashcard_tkinter.py
+++ flashcard_tkinter.py
@@ -1,30 +1,34 @@
-# a flashcard app using
-# tkinter
+"""Flashcard app using tkinter."""
 
 import tkinter as tk
 
-class Flashcard(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+def main():
+    """Displays a word and definition."""
+    class Flashcard(tk.Frame):
+        """Class docstring."""
+        def __init__(self, master=None):
+            super().__init__(master)
+            self.master = master
+            self.pack()
+            self.create_widgets()
 
-    def create_widgets(self):
-        self.greet = tk.Button(self)
-        self.greet['text'] = 'the word\n(click for the meaning)'
-        self.greet['command'] = self.say_hi
-        self.greet.pack(side = 'top')
+        def create_widgets(self):
+            """GUI window with button."""
+            self.greet = tk.Button(self)
+            self.greet['text'] = 'the word\n(click for the meaning)'
+            self.greet['command'] = self.say_hi
+            self.greet.pack(side='top')
 
-        self.quit = tk.Button(self, text='Quit', fg='red',
-                              command=self.master.destroy)
-        self.quit.pack(side='bottom')
+            self.quit = tk.Button(self, text='Quit', fg='red',
+                                  command=self.master.destroy)
+            self.quit.pack(side='bottom')
 
-    def say_hi(self):
-        print('hi there')
+        def say_hi(self):
+            """Label docstring."""
+            print('hi there')
 
-root = tk.Tk()
-app = Flashcard(master=root)
-app.mainloop()
+    root = tk.Tk()
+    app = Flashcard(master=root)
+    app.mainloop()
 
-print('done')
+main()
